3cec87e5a6a7c0bc2908ea0570e4a27a1d2646b9
Jorge Altamirano <alojorge13@gmail.com>
2026-03-05 11:56:41 -0300

    agregando ruta

--- src/api/request.api.jsx
+++ src/api/request.api.jsx
@@ -14,35 +14,35 @@ api.interceptors.request.use((config) => {
 });
 
 export const updateLoginApi = async (login) => {
-  return await api.post('/login', login)
+  return await api.post('/users/login', login)
 }
 
 export const sendDataNewUserApi = async (value) => {
-  return await api.post('/addUser', value)
+  return await api.post('users/addUser', value)
 }
 
 export const logOutApi = async () => {
-  return await api.post('/logout')
+  return await api.post('users/logout')
 }
 
 export const resetPasswordApi = async (password, token) => {
-  return await api.post(`/reset-password/${token}`, { password });
+  return await api.post(`/users/reset-password/${token}`, { password });
 }
 
 export const forgotEmailApi = async (email) => {
-  return await api.post('/forgotEmail', { email })
+  return await api.post('/users/forgotEmail', { email })
 }
 
 export const verifyTokenApi = async () => {
-  return await api.get(`/verify-token`);
+  return await api.get(`user/verify-token`);
 }
 
 export const getAllData = async (local) => {
   console.log("valor de userId en api", local);
-  return await api.get(`/comidas?local=${local}`);
+  return await api.get(`comidas/comidas?local=${local}`);
 }
 export const updateDataApi = async (data) => {
-  return await api.put('/update', data, {
+  return await api.put('comidas/update', data, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
@@ -50,7 +50,7 @@ export const updateDataApi = async (data) => {
 }
 
 export const cargarComidasApi = async (data) => {
-  return await api.post('/cargarcomidas', data, {
+  return await api.post('comidas/cargarcomidas', data, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
@@ -59,31 +59,31 @@ export const cargarComidasApi = async (data) => {
 
 export const destroyApi = async (id, opId) => {
   // Si opId existe, lo agregamos como parámetro de consulta
-  const url = opId ? `/destroy/${id}?opId=${opId}` : `/destroy/${id}`;
+  const url = opId ? `comidas/destroy/${id}?opId=${opId}` : `/destroy/${id}`;
   return await api.delete(url);
 }
 
 export const apiAddStandAll = async (categoria, data) => {
-  return await api.put('/standbyAll', { categoria, data });
+  return await api.put('comidas/standbyAll', { categoria, data });
 }
 
 export const sendPorcentageApi = async (data) => {
-  return await api.put('/porcentage', data);
+  return await api.put('comidas/porcentage', data);
 }
 
 export const updateUserDataApi = async ({value, id}) => {
-  return await api.put('/updateUser', {value, id});
+  return await api.put('users/updateUser', {value, id});
 }
 
 export const handleSetTimeApi = async (data) => {
-  return await api.put('/horario', data, {
+  return await api.put('restaurant/horario', data, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
   });
 }
 export const sendDataNewLocalApi = async (data) => {
-  return await api.put('/addLocal', data, {
+  return await api.put('restaurant/addLocal', data, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
@@ -91,25 +91,25 @@ export const sendDataNewLocalApi = async (data) => {
 }
 
 export const getUsersApi = async () => {
-  return await api.get("/users");
+  return await api.get("users/users");
 }
 
 export const getPreferencePayApi = async ({ userId }) => {
-  return await api.post('/preference', { userId });
+  return await api.post('mercado/preference', { userId });
 }
 
 export const checkPayAPi = async () => {
-  return await api.get("/checkPay");
+  return await api.get("users/checkPay");
 }
 
 export const getDataChartApi = async (localId, periodo) => {
-  return await api.get(`/chart/${localId}`, {
+  return await api.get(`pedidos/chart/${localId}`, {
     params: { periodo } // Esto generará: /chart/123?periodo=7d
   });
 }
 
 export const getLocalesApi = async () => {
-  return await api.get("/restaurantes");
+  return await api.get("restaurant/restaurantes");
 }
 
 
